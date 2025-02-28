6417a66bcd1ef369e5e58098b48afbdadbede942
marco-3636 <marcosiani01@gmail.com>
2025-02-28 16:54:05 +0100

    modifica test

--- LibraryProgram/src/main/java/com/swe/libraryprogram/domainmodel/Test.java
+++ LibraryProgram/src/main/java/com/swe/libraryprogram/domainmodel/Test.java
@@ -3,13 +3,13 @@ package com.swe.libraryprogram.domainmodel;
 public class Test {
 
     public static void main(String[] args) {
-        Book libro = new Book(1, "gino", 2010, "blablablablabla", 10, 2, 300, null, 12345, "me", "tu", 500);
+        Book libro = new Book("gino", 2010, "blablablablabla", 10, 2, 300, null, 12345, "me", "tu", 500);
         System.out.println(libro.toString());
 
-        DigitalMedia dm = new DigitalMedia(1, "gino", 2010, "blablablablabla", 10, 2, 300, null, "zio", 309, 2, "mio");
+        DigitalMedia dm = new DigitalMedia("gino", 2010, "blablablablabla", 10, 2, 300, null, "zio", 309, 2, "mio");
         System.out.println(dm.toString());
 
-        PeriodicPublication pp = new PeriodicPublication(1, "gino", 2010, "blablablablabla", 10, 2, 300, null, "zio", 309, 2, 2, 3333);
+        PeriodicPublication pp = new PeriodicPublication("gino", 2010, "blablablablabla", 10, 2, 300, null, "zio", 309, 2, 2, 3333);
         System.out.println(pp.toString());
     }
 
