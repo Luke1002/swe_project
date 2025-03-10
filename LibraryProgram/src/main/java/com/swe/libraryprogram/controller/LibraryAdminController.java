8f5d25f302c84c9899ab71deaf252ffb8cb3d579
Luca Lascialfari <lucalascialfari2001@gmail.com>
2025-03-10 19:26:38 +0100

    Implemented controller methods

--- LibraryProgram/src/main/java/com/swe/libraryprogram/controller/LibraryAdminController.java
+++ LibraryProgram/src/main/java/com/swe/libraryprogram/controller/LibraryAdminController.java
@@ -1,20 +1,101 @@
 package com.swe.libraryprogram.controller;
 
 
-import com.swe.libraryprogram.domainmodel.Element;
+import com.swe.libraryprogram.dao.*;
+import com.swe.libraryprogram.domainmodel.*;
 
+import java.sql.SQLException;
 
 
 public class LibraryAdminController {
 
 
-    //TODO: gestire statistiche biblioteca
+    private final ElementManager elementManager = new ElementManager();
+    private final BookManager bookManager = new BookManager();
+    private final DigitalMediaManager digitalMediaManager = new DigitalMediaManager();
+    private final PeriodicPublicationManager periodicPublicationManager = new PeriodicPublicationManager();
+    private final BorrowsManager borrowManager = new BorrowsManager();
+    private final ModifiedElements modifiedElemeents = new ModifiedElemeents();
+    private User usr;
+
+    LibraryAdminController(User usr) {
+        this.usr = usr;
+    }
 
 
 
-    public void addElement (Element element) {}
+    public Boolean addElement (Element element) {
+        ConnectionManager cM = ConnectionManager.getInstance();
+        try{
+            cM.getConnection().setAutoCommit(false);
+            if(element instanceof Book) {
+                bookManager.addBook((Book) element);
+                cM.getConnection().commit();
+            }
+            else if(element instanceof DigitalMedia) {
+                digitalMediaManager.addDigitalMedia((DigitalMedia) element);
+                cM.getConnection().commit();
+            }
+            else if(element instanceof PeriodicPublication) {
+                periodicPublicationManager.addPeriodicPublication((PeriodicPublication) element);
+                cM.getConnection().commit();
+            }
+            else{
+                cM.getConnection().setAutoCommit(true);
+                return false;
+            }
+            cM.getConnection().setAutoCommit(true);
+            return true;
+        } catch (SQLException e) {
+            try{
+                cM.getConnection().rollback();
+                cM.getConnection().setAutoCommit(true);
+            }catch (SQLException exception){
+                exception.printStackTrace();
+            }
+            return false;
+        }
+    }
 
-    public void removeElement (Element element) {}
+    public Boolean removeElement (Element element) {
+        try{
+            elementManager.removeElement(element.getId());
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public Boolean updateElement (Element element) {
+        ConnectionManager cM = ConnectionManager.getInstance();
+        try {
+            cM.getConnection().setAutoCommit(false);
+            if (element instanceof Book) {
+                bookManager.updateBook((Book) element);
+            } else if (element instanceof DigitalMedia) {
+                digitalMediaManager.updateDigitalMedia((DigitalMedia) element);
+            } else if (element instanceof PeriodicPublication) {
+                periodicPublicationManager.updatePeriodicPublication((PeriodicPublication) element);
+            } else {
+                cM.getConnection().setAutoCommit(true);
+                return false;
+            }
+            elementManager.updateElement(element);
+            modifiedElemeents.addEdit(element.getId(), usr.getEmail());
+            cM.getConnection().commit();
+            cM.getConnection().setAutoCommit(true);
+            return true;
+        } catch (SQLException e) {
+            try{
+                cM.getConnection().rollback();
+                cM.getConnection().setAutoCommit(true);
+            }catch (SQLException exception){
+                exception.printStackTrace();
+            }
+            return false;
+        }
+    }
 
     public void getLibraryStatistics () {}
 
--- LibraryProgram/src/main/java/com/swe/libraryprogram/controller/LibraryUserController.java
+++ LibraryProgram/src/main/java/com/swe/libraryprogram/controller/LibraryUserController.java
@@ -1,27 +1,76 @@
 package com.swe.libraryprogram.controller;
 
+import com.swe.libraryprogram.dao.ConnectionManager;
 import com.swe.libraryprogram.dao.UserManager;
 import com.swe.libraryprogram.domainmodel.Element;
 import com.swe.libraryprogram.dao.ElementManager;
 import com.swe.libraryprogram.dao.BorrowsManager;
+import com.swe.libraryprogram.domainmodel.User;
+import org.postgresql.util.PGJDBCMain;
 
+import java.sql.SQLException;
 
 
 public class LibraryUserController {
 
     private final ElementManager elementManager = new ElementManager();
     private final BorrowsManager borrowManager = new BorrowsManager();
+    private User usr;
 
-    //TODO: gestire statistiche biblioteca
+    LibraryUserController(User usr) {
+        this.usr = usr;
+    }
 
+    public Boolean borrowElement (Integer element_id) {
+        ConnectionManager cM = ConnectionManager.getInstance();
+        try{
+            cM.getConnection().setAutoCommit(false);
+            Element element = elementManager.getElement(element_id);
+            if (element!=null && element.getQuantityAvailable()>0) {
+                element.setQuantityAvailable(element.getQuantityAvailable()-1);
+                elementManager.updateElement(element);
+                borrowManager.addBorrow(element_id, usr.getEmail());
+                cM.getConnection().commit();
+                cM.getConnection().setAutoCommit(true);
+                return true;
+            }
+            return false;
+        }catch (SQLException e){
+            try{
+                cM.getConnection().rollback();
+                cM.getConnection().setAutoCommit(true);
+            }catch (SQLException exception){
+                exception.printStackTrace();
+            }
+            return false;
+        }
+    }
 
+    public Boolean returnElement (Integer element_id) {
+        ConnectionManager cM = ConnectionManager.getInstance();
+        try{
+            cM.getConnection().setAutoCommit(false);
+            Element element = elementManager.getElement(element_id);
+            if (element!=null && element.getQuantityAvailable()<element.getQuantity()) {
+                element.setQuantityAvailable(element.getQuantityAvailable()+1);
+                elementManager.updateElement(element);
+                borrowManager.removeBorrow(element_id, usr.getEmail());
+                cM.getConnection().commit();
+                cM.getConnection().setAutoCommit(true);
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            try{
+                cM.getConnection().rollback();
+                cM.getConnection().setAutoCommit(true);
+            }catch (SQLException exception){
+                exception.printStackTrace();
+            }
+            return false;
+        }
+    }
 
-    public Boolean borrowElement (String element_id) {return null;}
-
-    public Boolean returnElement (Element element) {return null;}
-
-    public void getPersonalStatistics () {}
-
-    public void reportElement () {}
+    // public void reportLostElement (Integer element_id) {}
 
 }
