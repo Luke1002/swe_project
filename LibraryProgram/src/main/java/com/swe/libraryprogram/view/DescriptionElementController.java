af9f96fc8a7f9b166e1255c8a2c97f9afaffc0f8
Luca Lascialfari <lucalascialfari2001@gmail.com>
2025-03-19 20:09:20 +0100

    Updated Description Element View

--- LibraryProgram/src/main/java/com/swe/libraryprogram/view/DescriptionElementController.java
+++ LibraryProgram/src/main/java/com/swe/libraryprogram/view/DescriptionElementController.java
@@ -68,19 +68,13 @@ public class DescriptionElementController extends BaseViewController {
 
     private Scene previousScene;
 
-    private User user;
-
     private Element element;
-
-    private UserController userController;
-
-    private ElementManager elementManager;
 
 
     @FXML
     protected void initialize() {
         super.initialize();
-        elementManager = new ElementManager();
+        element = MainController.getInstance().getElementManager().getCompleteElementById(MainController.getInstance().getSelectedElementId());
 
         action1.setOnAction(event -> handleAction1());
         action2.setOnAction(event -> handleAction2());
@@ -93,8 +87,6 @@ public class DescriptionElementController extends BaseViewController {
         ratingSlider.valueProperty().addListener((obs, oldVal, newVal) ->
                 ratingText.setText(String.format("%.1f", newVal))
         );
-
-        welcomeText.setText("Benvenuto, " + user.getName() + "!");
 
         //imposta la vista in base al tipo di elemento
         updateView();
@@ -110,15 +102,13 @@ public class DescriptionElementController extends BaseViewController {
         System.out.println("Azione 2 selezionata.");
     }
 
-    private void handleTakeAction() {
+    private void handleBorrowAction() {
 
-        LibraryUserController userController = (LibraryUserController) this.userController;
-
-        if (userController.borrowElement(element.getId())) {
-            showAlert("Prestito dell'elemento", "Elemento preso!", AlertType.INFORMATION);
+        if (((LibraryUserController)MainController.getInstance().getUserController()).borrowElement(element.getId())) {
+            showAlert("Prestito dell'elemento", "Elemento preso!");
 
         } else {
-            showAlert("Prestito dell'elemento", "Errore durante il prestito dell'elemento", AlertType.ERROR);
+            showAlert("Prestito dell'elemento", "Errore durante il prestito dell'elemento");
 
         }
 
@@ -134,173 +124,19 @@ public class DescriptionElementController extends BaseViewController {
 
         try {
 
-            if (elementManager.removeElement(element.getId())) {
+            if (MainController.getInstance().getElementManager().removeElement(element.getId())) {
 
-                showAlert("Rimozione dell'elemento", "Elemento rimosso con successo", AlertType.INFORMATION);
-                goBack();
+                showAlert("Rimozione dell'elemento", "Elemento rimosso con successo");
+
 
             }
 
         } catch (SQLException e) {
 
-            showAlert("Rimozione dell'elemento", "Errore durante la rimozione dell'elemento", AlertType.ERROR);
+            showAlert("Rimozione dell'elemento", "Errore durante la rimozione dell'elemento");
             e.printStackTrace();
 
         }
 
     }
-
-    private void updateView() {
-
-        if (user.isAdmin()) {
-
-            editButton.setVisible(true);
-            removeButton.setVisible(true);
-            takeButton.setVisible(false);
-
-        } else {
-
-            editButton.setVisible(false);
-            removeButton.setVisible(false);
-            takeButton.setVisible(true);
-
-        }
-
-        if (element instanceof Book) {
-
-            authorText.setVisible(true);
-            publisherText.setVisible(true);
-            genresText.setVisible(true);
-            ISBNText.setVisible(true);
-
-            producerText.setVisible(false);
-            ageRatingText.setVisible(false);
-            directorText.setVisible(false);
-
-            frequencyText.setVisible(false);
-            releaseMonthText.setVisible(false);
-            releaseDayText.setVisible(false);
-            ISSNText.setVisible(false);
-
-        } else if (element instanceof DigitalMedia) {
-
-            producerText.setVisible(true);
-            ageRatingText.setVisible(true);
-            directorText.setVisible(true);
-
-            authorText.setVisible(false);
-            publisherText.setVisible(false);
-            genresText.setVisible(false);
-
-            frequencyText.setVisible(false);
-            releaseMonthText.setVisible(false);
-            releaseDayText.setVisible(false);
-            ISSNText.setVisible(false);
-
-        } else if (element instanceof PeriodicPublication) {
-
-            frequencyText.setVisible(true);
-            releaseMonthText.setVisible(true);
-            releaseDayText.setVisible(true);
-            ISSNText.setVisible(true);
-            publisherText.setVisible(true);
-
-            authorText.setVisible(false);
-            genresText.setVisible(false);
-
-            producerText.setVisible(false);
-            ageRatingText.setVisible(false);
-            directorText.setVisible(false);
-
-        }
-
-    }
-
-    public void setUser(User user) {
-
-        if (this.user != null && this.user.equals(user)) {
-
-            System.out.println("Utente già impostato.");
-            return;
-
-        }
-
-        this.user = user;
-
-        if (user.isAdmin()) {
-            userController = new LibraryAdminController();
-
-        } else {
-            userController = new LibraryUserController();
-
-        }
-
-    }
-
-    public void setElement(Element element) {
-
-        this.element = element;
-
-        titleText.setText(element.getTitle());
-        yearText.setText(element.getReleaseYear().toString());
-        lengthText.setText(element.getLength().toString());
-        genresText.setText(element.getGenres().toString());
-
-        descriptionFlow.getChildren().add(new Text(element.getDescription()));
-
-        if (element instanceof Book) {
-
-            Book book = (Book) element;
-
-            ISBNText.setText(book.getIsbn().toString());
-            authorText.setText(book.getAuthor());
-            publisherText.setText(book.getPublisher());
-            editionText.setText(book.getEdition().toString());
-
-        } else if (element instanceof DigitalMedia) {
-
-            DigitalMedia digitalMedia = (DigitalMedia) element;
-
-            producerText.setText(digitalMedia.getProducer());
-            ageRatingText.setText(digitalMedia.getAgeRating().toString());
-            directorText.setText(digitalMedia.getDirector());
-
-        } else if (element instanceof PeriodicPublication) {
-
-            PeriodicPublication periodicPublication = (PeriodicPublication) element;
-
-            frequencyText.setText(periodicPublication.getFrequency().toString());
-            releaseMonthText.setText(periodicPublication.getReleaseMonth().toString());
-            releaseDayText.setText(periodicPublication.getReleaseDay().toString());
-            ISSNText.setText(periodicPublication.getIssn().toString());
-            publisherText.setText(periodicPublication.getPublisher());
-
-        }
-
-    }
-
-    private void showAlert(String title, String message, AlertType type) {
-
-        Alert alert = new Alert(type);
-        alert.setTitle(title);
-        alert.setHeaderText(null);
-        alert.setContentText(message);
-        alert.showAndWait();
-
-    }
-
-    private void goBack() {
-
-        Stage stage = (Stage) returnButton.getScene().getWindow();
-        stage.setScene(previousScene);
-        stage.show();
-
-    }
-
-    private void setPreviousScene(Scene scene) {
-        this.previousScene = scene;
-
-    }
-
-
 }
