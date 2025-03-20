0ed77bcda1ccd4bf28767233969924dd8a5b0c5d
Luca Lascialfari <lucalascialfari2001@gmail.com>
2025-03-20 12:08:32 +0100

    Added check to UserController

--- LibraryProgram/src/main/java/com/swe/libraryprogram/controller/UserController.java
+++ LibraryProgram/src/main/java/com/swe/libraryprogram/controller/UserController.java
@@ -10,15 +10,22 @@ public class UserController {
     private final UserManager userManager = new UserManager();
 
     public User login (String email, String password) throws SQLException {
+        if (email == null || password == null || email.isEmpty() || password.isEmpty()){
+            return null;
+        }
         return userManager.authenticate(email, password);
     }
 
     public Boolean signup (String email, String password, String name, String surname, String phone) {
+        if(email == null || password == null || name == null || surname == null ||email.isEmpty() || password.isEmpty() || name.isEmpty() || surname.isEmpty() ) {
+            return false;
+        }
         try{
             if(userManager.getUser(email) != null){
                 return false;
             }
             else{
+
                 return userManager.addUser(new User(email, password, name, surname, phone));
             }
         }
@@ -28,6 +35,6 @@ public class UserController {
     }
 
     public void logout(){
-        MainController.getInstance().resetState();
+        MainController.getInstance().resetUserState();
     }
 }
