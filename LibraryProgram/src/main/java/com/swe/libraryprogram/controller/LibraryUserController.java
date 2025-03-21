ef8f1b547fc8e0c823b9ffdcfa85fb6b49ae0f05
Luca Lascialfari <lucalascialfari2001@gmail.com>
2025-03-21 12:04:21 +0100

    Removed redundant instantiation

--- LibraryProgram/src/main/java/com/swe/libraryprogram/controller/LibraryUserController.java
+++ LibraryProgram/src/main/java/com/swe/libraryprogram/controller/LibraryUserController.java
@@ -14,42 +14,37 @@ public class LibraryUserController extends UserController {
     public LibraryUserController() {
     }
 
-    public Boolean borrowElement (Integer element_id) {
-        ConnectionManager cM = ConnectionManager.getInstance();
-        try{
+    public Boolean borrowElement(Integer element_id) {
+        try {
             Element element = MainController.getInstance().getElementManager().getElement(element_id);
-            if (element!=null && element.getQuantityAvailable()>0) {
-                element.setQuantityAvailable(element.getQuantityAvailable()-1);
+            if (element != null && element.getQuantityAvailable() > 0) {
+                element.setQuantityAvailable(element.getQuantityAvailable() - 1);
                 MainController.getInstance().getElementManager().updateElement(element);
                 MainController.getInstance().getBorrowsManager().addBorrow(element_id, MainController.getInstance().getUser().getEmail());
                 return true;
             }
             return false;
-        }catch (SQLException e){
-            try{
-                cM.getConnection().rollback();
-            }catch (SQLException exception){
-                exception.printStackTrace();
-            }
+        } catch (SQLException e) {
+            e.printStackTrace();
             return false;
         }
     }
 
-    public Boolean returnElement (Integer element_id) {
+    public Boolean returnElement(Integer element_id) {
         ConnectionManager cM = ConnectionManager.getInstance();
-        try{
+        try {
             Element element = MainController.getInstance().getElementManager().getElement(element_id);
-            if (element!=null && element.getQuantityAvailable()<element.getQuantity()) {
-                element.setQuantityAvailable(element.getQuantityAvailable()+1);
+            if (element != null && element.getQuantityAvailable() < element.getQuantity()) {
+                element.setQuantityAvailable(element.getQuantityAvailable() + 1);
                 MainController.getInstance().getElementManager().updateElement(element);
                 MainController.getInstance().getBorrowsManager().removeBorrow(element_id, MainController.getInstance().getUser().getEmail());
                 return true;
             }
             return false;
         } catch (SQLException e) {
-            try{
+            try {
                 cM.getConnection().rollback();
-            }catch (SQLException exception){
+            } catch (SQLException exception) {
                 exception.printStackTrace();
             }
             return false;
