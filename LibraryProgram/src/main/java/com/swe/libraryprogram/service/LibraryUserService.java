26f325d219273dd2beabe87779cd34d7d644deea
Luca Lascialfari <lucalascialfari2001@gmail.com>
2025-03-22 19:45:11 +0100

    Corrected error

--- LibraryProgram/src/main/java/com/swe/libraryprogram/service/LibraryUserService.java
+++ LibraryProgram/src/main/java/com/swe/libraryprogram/service/LibraryUserService.java
@@ -16,6 +16,9 @@ public class LibraryUserService extends UserService {
     public List<Element> getBorrowedElements(User user) {
         try {
             List<Element> borrowedElements = MainService.getInstance().getBorrowsDAO().getBorrowedElementsForUser(user.getEmail());
+            if(borrowedElements.isEmpty()) {
+                System.out.println("Nessun elemento preso in prestito.");
+            }
             return borrowedElements;
         } catch (SQLException e) {
             System.out.println("Impossibile connettersi al database.");
@@ -26,6 +29,7 @@ public class LibraryUserService extends UserService {
     public Boolean borrowElement(Integer elementId) {
         if (elementId == null) {
             System.out.println("Elemento non selezionato");
+            return false;
         }
         try {
             Element element = MainService.getInstance().getElementDAO().getElement(elementId);
@@ -38,9 +42,9 @@ public class LibraryUserService extends UserService {
                 System.out.println("Elemento non disponibile per il prestito.");
                 return false;
             }
-                MainService.getInstance().getElementDAO().updateElement(element);
-                MainService.getInstance().getBorrowsDAO().addBorrow(elementId, MainService.getInstance().getUser().getEmail());
-                return true;
+            MainService.getInstance().getElementDAO().updateElement(element);
+            MainService.getInstance().getBorrowsDAO().addBorrow(elementId, MainService.getInstance().getUser().getEmail());
+            return true;
         } catch (SQLException e) {
             System.out.println("Impossibile connettersi al database.");
         } catch (NoSuchElementException e) {
@@ -49,16 +53,25 @@ public class LibraryUserService extends UserService {
         return false;
     }
 
-    public Boolean returnElement(Integer element_id) {
-        try {
-            Element element = MainService.getInstance().getElementDAO().getElement(element_id);
-            if (element != null && element.getQuantityAvailable() < element.getQuantity()) {
-                element.setQuantityAvailable(element.getQuantityAvailable() + 1);
-                MainService.getInstance().getElementDAO().updateElement(element);
-                MainService.getInstance().getBorrowsDAO().removeBorrow(element_id, MainService.getInstance().getUser().getEmail());
-                return true;
-            }
+    public Boolean returnElement(Integer elementId) {
+        if (elementId == null) {
+            System.out.println("Elemento non selezionato");
             return false;
+        }
+        try {
+            Element element = MainService.getInstance().getElementDAO().getElement(elementId);
+            List<Element> elements = getBorrowedElements(MainService.getInstance().getUser());
+            if (!elements.contains(element)) {
+                System.out.println("Elemento non preso in prestito.");
+                return false;
+            }
+            if (element.getQuantityAvailable() >= element.getQuantity()) {
+                element.setQuantityAvailable(element.getQuantityAvailable() - 1);
+                MainService.getInstance().getElementDAO().updateElement(element);
+            }
+
+            MainService.getInstance().getBorrowsDAO().removeBorrow(elementId, MainService.getInstance().getUser().getEmail());
+            return true;
         } catch (SQLException e) {
             System.out.println("Impossibile connettersi al database.");
             return false;
